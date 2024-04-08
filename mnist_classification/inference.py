d684b95cef86a88c6a0fc371ec3536cf02a18f38
Darshan Patil <darshanpatil05092001@gmail.com>
2024-04-08 12:44:04 +0530

    deployment file created

--- mnist_classification/inference.py
+++ mnist_classification/inference.py
@@ -1,20 +1,41 @@
 import tensorflow as tf
+import numpy as np
 from flask import Flask, request, jsonify
+from PIL import Image
+import io
 
-model = tf.keras.models.load_model("models")
+model = tf.keras.models.load_model("models/data/model")
 
 app = Flask(__name__)
 
-@app.route("/predict", methods=["POST"])
+def preprocess_image(image_file):
+    img = Image.open(io.BytesIO(image_file.read()))
+    img = img.resize((28, 28))
+    img = img.convert('L')
+    img_array = np.array(img)
+    img_array = img_array / 255.0
+    img_array = np.expand_dims(img_array, axis=0)
+    img_array = np.expand_dims(img_array, axis=3)
+    return img_array
+
+@app.route("/predict", methods=["GET"])
 def predict():
-    data = request.json['image']
-    data = tf.convert_to_tensor(data)
-    data = tf.expand_dims(data, axis=0)
+    if 'image' not in request.files:
+        return jsonify({"error": "No image uploaded"}), 400
+
+    image_file = request.files['image']
+
+    if image_file.filename == '':
+        return jsonify({"error": "Empty filename"}), 400
+
+    try:
+        img_array = preprocess_image(image_file)
+        predictions = model.predict(img_array)
+        predicted_label = np.argmax(predictions)
+        return jsonify({"predicted_label": int(predicted_label)})
     
-    predictions = model.predict(data)
-    predicted_label = tf.argmax(predictions, axis=1).numpy()[0]
-    
-    return jsonify({"predicted_label": int(predicted_label)})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
